af821b744e2b52de23830348a8c038d7cdfd98d1
Hamza <hamza.k.ammar@gmail.com>
2026-01-03 17:38:06 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Swim Timesheet API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
